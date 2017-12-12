c80b587f872ede9180b70ab067e43c06181e8c66
tavo1987 <tavo198718@gmail.com>
2017-12-12 16:36:54 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h1>Modal example</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ullam id fuga, rerum laborum quibusdam inventore, nemo in velit voluptatibus ipsum quis dolorem ab nulla. Dignissimos nam reiciendis animi dolor.
+    </p>
+    <button href="#" @click.prevent="$refs.welcome.open()">Open Welcome Modal</button>
+    <button href="#" @click.prevent="$refs.other.open()">Open Other Modal</button>
+
+    <modal ref="welcome">
+      <h2>Welcome</h2>
+    </modal>
+
+    <modal ref="other">
+      <h2>Other Modal</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. In, nobis voluptatem? Beatae esse mollitia veniam modi fugiat accusamus, exercitationem animi odio consequuntur id iure quae ad nisi quidem, suscipit nesciunt?
+      </p>
+    </modal>
+
+  </div>
+</template>
+
+<script>
+import Modal from './components/modal.vue'
+export default {
+  name: 'app',
+  components: {
+    Modal
+  },
+  mounted() {
+    console.log(this.$refs.loginModal);
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/modal.vue
@@ -0,0 +1,65 @@
+<template>
+    <transition
+        enter-active-class="animated fadeIn"
+        leave-active-class="animated zoomOut">
+        <div class="modal" v-if="isActive">
+            <div class="modal__content">
+                <slot></slot>
+            </div>
+            <span class="modal__close" @click="close()">
+                close
+            </span>
+        </div>
+    </transition>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                isActive: false
+            }
+        },
+        methods: {
+            open() {
+                this.isActive = true;
+            },
+            close() {
+                this.isActive = false
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .modal {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background: rgba(black, .3);
+        z-index: 1000;
+        &__content {
+            background: white;
+            padding:1rem 2rem;
+            border-radius:10px;
+            max-width: 800px;
+            margin-right: 1rem;
+            margin-left: 1rem;
+        }
+        &__close {
+            position: absolute;
+            right: 1rem;
+            top: 1rem;
+            background: seagreen;
+            padding: .5rem 1rem;
+            color: white;
+            border-radius: 3px;
+        }
+    }
+</style>
+
